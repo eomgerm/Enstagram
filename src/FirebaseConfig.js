106538fbf7d2205bf1b281e38285dc953f9d5fe3
엄기훈 <rlgns0705@naver.com>
2021-12-11 09:35:59 +0000

    editing some dependencies

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import {getStorage} from 'firebase/storage';
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,3 +19,5 @@ const firebaseConfig = {
 // Initialize Firebase
 initializeApp(firebaseConfig);
 export const authService = getAuth();
+export const fsService = getFirestore();
+export const storageService = getStorage();
